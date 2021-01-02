a2f2c0847032433f7d205d64cc9850ff3fc88ab9
Marcus Rahm <marcus.rahm@octacon.se>
2021-01-02 11:11:02 +0100

    Add project files.

---
+++ ServerLauncherBot/Program.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace ServerLauncherBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            var token = "k8G0bJ9s1xktZzFgbV4L4fKO4K8jafon";
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
